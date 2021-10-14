8c373d0b6b61031753376bed3d2932f5eb908cc3
raddie1 <kalejayeiradahtolani@gmail.com>
2021-10-14 15:18:11 +0100

    Feat: Completed the most part of the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,100 @@
+import { useState, useEffect } from 'react';
 import './css/style.css';
+import dataSet from './assets/data.json';
+import JobCard from './components/JobCard';
 
 function App() {
+  const [data, setData] = useState(dataSet);
+  
+  const [filters, setFilters] = useState([]);
+
+  function addFilter (filter) {
+    setFilters([...filters, filter]);
+  }
+
+  function removeFilter (filter) {
+    let newFiltersList = filters.filter(item => item !== filter);
+    newFiltersList.forEach(filter => {
+      sortByRole(filter, dataSet);
+      sortByLevel(filter, dataSet);
+      sortByLanguages(filter, dataSet);
+      sortByTools(filter, dataSet);
+    })
+
+    if (newFiltersList.length === 0) {
+      setData(dataSet)
+    }
+
+    setFilters(newFiltersList);
+
+  }
+
+  function clearFilter () {
+    setFilters([]);
+    setData(dataSet);
+  }
+
+function sortByRole (role, dataSet) {
+  let newList =(dataSet ?? data).filter(job => job.role === role);
+  setData(newList);
+}
+
+function sortByLevel (level, dataSet) {
+  let newList = (dataSet ?? data).filter(job => job.level === level);
+  setData(newList);
+}
+
+function sortByLanguages (language, dataSet) {
+  let newList = (dataSet ?? data).filter(job => job.languages.includes(language));
+  setData(newList)
+}
+
+function sortByTools (tool, dataSet) {
+  let newList = (dataSet ?? data).filter(job => job.tools.includes(tool));
+  setData(newList);
+}
+  useEffect(() => {
+    console.log(dataSet);
+  }, [])
   return (
     <div className="App">
-   
+      <header>
+        {filters.length !==0 &&
+          <div className="filter-block" hidden={filters.length <= 0}>
+          {
+            filters.map((item, index) => {
+              return (
+                <div className="filter" key={index}>
+                  <span>{item}</span>
+                  <button onClick={() => removeFilter(item)}>X</button>
+
+                </div>
+              )
+            })
+             
+          }
+          <button onClick={clearFilter}>clear</button>
+        </div>}
+      </header>
+      <main>
+        {
+          data.map((job, index) => {
+            return (
+              <JobCard
+               data={job}
+               actions={{
+                 sortByRole,
+                 sortByLevel,
+                 sortByLanguages,
+                 sortByTools,
+                 addFilter,
+               }}
+               key={index}
+               />
+            )
+          })
+        }
+      </main>
     </div>
   );
 }
---
+++ src/components/images.js
@@ -0,0 +1,28 @@
+import photosnap from '../assets/images/photosnap.svg'
+import manage from '../assets/images/manage.svg'
+import account from '../assets/images/account.svg'
+import myhome from '../assets/images/myhome.svg'
+import loopStudios from '../assets/images/loop-studios.svg'
+import faceIt from '../assets/images/faceit.svg'
+import shortly from '../assets/images/shortly.svg'
+import insure from '../assets/images/insure.svg'
+import eyecamCo from '../assets/images/eyecam-co.svg'
+import theAirFilterComapny from '../assets/images/the-air-filter-company.svg'
+
+const allImages = {
+    photosnap: photosnap,
+    manage: manage,
+    account: account,
+    myhome: myhome,
+    'loop-studios': loopStudios,
+    faceit: faceIt,
+    shortly: shortly,
+    insure: insure,
+    'eyecam-co': eyecamCo,
+    'the-air-filter-company':theAirFilterComapny,
+}
+
+export default allImages
+
+
+
